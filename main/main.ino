a6aa210098127605289514aec10ade5f7f6783af
Tr1ck4 <minhbao11122003@gmail.com>
2026-02-25 16:47:26 +0100

    Migration, Adding infrared

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,14 @@
 #include "movement.c"
+<<<<<<< Updated upstream
 #include "sensor.c"
 #include <SoftwareSerial.h>
 SoftwareSerial BT(2, 3);
+=======
+#include "infra.c"
+>>>>>>> Stashed changes
 
 void setup() {
-
+  Serial.begin(9600);
   pinMode(ENA_ALL, OUTPUT);
   
   pinMode(IN1_F, OUTPUT);
@@ -16,9 +20,32 @@ void setup() {
   pinMode(IN2_R, OUTPUT);
   pinMode(IN3_R, OUTPUT);
   pinMode(IN4_R, OUTPUT);
+
+  pinMode(Pin_left_ir, INPUT);
+  pinMode(Pin_right_ir, INPUT);
 }
 
+// void loop() {
+//   // turnLeft();
+//   // delay(1200);
+//   // stopMotors();
+//   // delay(1000);
+//   int L = digitalRead(Pin_left_ir);
+//   int R = digitalRead(Pin_right_ir);
+//   Serial.print(L);
+//   Serial.print(" ");
+//   Serial.println(R);
+//   int result = detech_line();
+//   // Serial.println(result);
+//   switch (result) {
+//     case 0:
+//       stopMotors();
+//       break;
 
+//     case 2:
+//       // code for result == 1
+//       turnRight();
+//       break;
 
 //     case 1:
 //       // code for result == 2
